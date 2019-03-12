d18905571503fe3576f3954271b9514b0056671f
Juraj Trappl <juro.trappl@gmail.com>
2019-03-12 23:39:22 +0100

    Add file

---
+++ tra.sh
@@ -0,0 +1,12 @@
+Obsluha databazy telefonickej spolocnosti
+
+Mame databazu, ktora je v ulozena v skrytom subore napriklad .telDb.
+Databaza obsahuje meno a priezvisko (prijmeni) zakaznika, statistiku volani
+(prevolane minuty, smsky), datum vzniku zmluvy a ukoncenie platnosti,
+pokojne aj dalsie veci ako napriklad tarif, typ sluzby..
+
+Ovladanie si predstavujem pomocou nejakych prepinacov, napr.
+tel.sh -help -> vypise mozne prikazy
+tel.sh -existing -> vypise existujuce zmluvy
+tel.sh -show "meno prijmeni" -> vypise informacie o danej osobe
+. . .
