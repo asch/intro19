780b0c2fbee6967c6f002d9ed96f9c6388323e70
ptakopysk1 <47824225+ptakopysk1@users.noreply.github.com>
2019-03-13 11:48:50 +0100

    Create $klv.sh

---
+++ $klv.sh
@@ -0,0 +1,5 @@
+vytvořit skript, který provádí standardní skalární součin dvou vektorů v souboru sss.txt zadané ve formátu
+x = (0,0,0,...,0)
+y = (0,0,0,...,0)
+a výsledek zapíše na konec souboru ve formátu
+x*y = 
