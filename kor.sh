55584c961601c3f7c2f65b39aeb9cfcb5cfe0ee2
Tomáš Korený <pumaslab@gmail.com>
2019-03-13 17:15:38 +0100

    Added task for T.Koreny

---
+++ kor.sh
@@ -0,0 +1,3 @@
+#Skript který bude řešit Hanoiské věže pro n disků podle vstupu, 
+#vypíše postup na výstup, jeden řádek = 1 pohyb disku, musí tedy
+#vypsat číslo disku a číslo z jaké tyče na kterou ho přesunu
