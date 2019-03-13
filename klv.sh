b2ee7c3d105403b309b8136c7e30c007106b8b1b
ptakopysk1 <47824225+ptakopysk1@users.noreply.github.com>
2019-03-13 11:35:02 +0100

    Create klv.sh

---
+++ klv.sh
@@ -0,0 +1,6 @@
+Skript který provádí skalární součin dvou vektorů zadané v souboru ss.txt ve formátu:
+x = (0,0,...,0)
+y = (0,0,...,0)
+a vytiskne výsledek ve formátu 
+x * y = 
+na konec souboru (na nový řádek)
