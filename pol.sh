59eb71b89c04fc141fe5b2e82d84b69645115097
apolicky <42609304+apolicky@users.noreply.github.com>
2019-03-13 10:28:56 +0100

    Add files via upload
    Policky - pol.sh

---
+++ pol.sh
@@ -0,0 +1,13 @@
+Jste ucitel a mate na starost cviceni predmetu se tremi studenty. Kazdou hodinu si napisete opacko o 20 otazkach typu A/B/C, pouze 1 spravna odpoved. Spravne odpovedi i odpovedi zaku se pripisuji automaticky vzdy na konec souboru, napr. predmet.txt.
+
+Soubor je formatovany takto:
+1. radek:
+  .sprOdp(\t)	zakX(\t) zakY(\t) zakZ(\t)
+dalsi radky:
+	A	A	 B	  A
+
+	...
+
+Po dopsani kazdeho testu byste rad vedel, kolik chyb mel kazdy jeden zak v danem opacku a kolik chyb ma za semestr dohromady, oddeleno treba ":", v dalsim radku prislusneho sloupce.
+
+Vas skript se spusti az po prvnim opacku, kdy soubor uz bude mit sloupce jako nahore. Odpovedi studentu z dalsich opacek se budou opet automaticky pripisovat za vase vypocty.
