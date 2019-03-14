8aaf4f9626ae59b6462d79b8cac4d222eefea378
leskovde <47887202+leskovde@users.noreply.github.com>
2019-03-14 22:36:01 +0100

    Add files via upload

---
+++ les.sh
@@ -0,0 +1,1 @@
+Zkopirujte vsechny soubory zacinajici pismenem "n" ze slozky /usr/include do slozky v domacim adresari, ktera bude mit jmeno podle posledne upraveneho souboru z tech, ktere kopirujeme.
