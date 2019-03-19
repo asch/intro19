5cdaa29efb7802ea1ae93f4f1d30f2bfd6e28681
sauermar <47824190+sauermar@users.noreply.github.com>
2019-03-19 12:15:51 +0100

    create sau.sh

---
+++ sau.sh
@@ -0,0 +1,4 @@
+Vytvořte dva různé adresáře raz a dva a zkopírujte do nich stejnou
+skupinu souborů. V obou adresářích vymažte některé soubory (různé
+od sebe v každém z nich). Napiště skript, který do obou zkopíruje 
+z opačného adresáře smazané soubory.
