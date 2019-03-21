ab75ed8e155fc7953dabe69d7695335244a4cebc
leskovde <47887202+leskovde@users.noreply.github.com>
2019-03-21 22:16:49 +0100

    Update les.sh

--- les.sh
+++ les.sh
@@ -1,8 +1,1 @@
-#Skript dostane argumenty, ktere vzdy po dvojicich, ktere specifikuji, jak se ma soubor prejmenovat.
-#Prvni argument je stary nazev souboru a druhy argument je novy nazev.
-#
-#Pr.: ./les.sh stary novy old new antigua neuva alt neu vieux nouveau
-#ls predtim: stary, old, antigua, alt, vieux
-#ls potom: novy, new, neuva, neu, nouveau
-#
-#Skript bude osetren proti lichym a nulovym poctum argumentu (chybova hlaska).
+ Zkopirujte vsechny soubory zacinajici pismenem "n" ze slozky /usr/include do slozky v domacim adresari, ktera bude mit jmeno podle posledne upraveneho souboru z tech, ktere kopirujeme. 
