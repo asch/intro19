fd01e9a5e0e8c925c7ccbc2881b4bb29e8193a9b
Walkamron <47887217+Walkamron@users.noreply.github.com>
2019-03-22 06:27:38 +0100

    Update let.sh

--- let.sh
+++ let.sh
@@ -1,1 +1,1 @@
-Vytvořit skript, který z adresáře vypíše názvy všech neskrytých souborů v abecedním pořadí do souboru a poté všech skrytých souborů do jiného souboru
+Vytvořit skript, který z adresáře vypíše názvy všech neskrytých souborů v abecedním pořadí do souboru a poté všech skrytých souborů, opět v abecedním pořadí, do jiného souboru
