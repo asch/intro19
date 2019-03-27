fdbbb53154767dcf1834b22e7cde54e5b4951a02
NodariGogatishvili <35267253+NodariGogatishvili@users.noreply.github.com>
2019-03-27 10:35:41 +0100

    Create gog2.sh

---
+++ gog2.sh
@@ -0,0 +1,8 @@
+Write dash script which will write it own code to your personal web page on Matfyz http://www.ms.mff.cuni.cz/~USERNAME/intro19.html
+write your USERNAME which you use for login in lab or SIS
+You must set up your web page firts, this may help you http://wiki.ms.mff.cuni.cz/wiki/laborato%C5%99_UNIX#WWW_str%C3%A1nky
+Script will also read from this file on git to set font-size which will be solution of y=5+12/2+2*5 in pixels.
+Script must heve 2 paramters first output adsress and second color of code in html
+./NAME.sh [ address ] [ color ] 
+It must accept any valid html/css color definition.
+And your html file must containe css style body {background: black} //read from from this file on git
